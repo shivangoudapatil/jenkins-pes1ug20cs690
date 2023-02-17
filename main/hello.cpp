862c01a4beeac0296dcad34e2304443716b34bd5
Shivangouda Patil <91386977+shivangoudapatil@users.noreply.github.com>
2023-02-17 19:21:18 +0530

    Update hello.cpp
    new change

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "I have made these changes , my srn is pes1ug20cs690\n";
+  cout << "this is change , my srn is pes1ug20cs690\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
   return 0;
